d1c81255c6d22d2e5c74872756606470058f03ae
russelhanapi <russelaminkadraphanapi@iskolarngbayan.pup.edu.ph>
2023-10-05 01:25:21 +0800

    Integrate Google Map

--- app/src/main/java/com/example/rescyou/Home.kt
+++ app/src/main/java/com/example/rescyou/Home.kt
@@ -6,71 +6,101 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.rescyou.databinding.ActivityHomeBinding
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
 
 
+class Home : AppCompatActivity(), OnMapReadyCallback {
 
-private lateinit var binding: ActivityHomeBinding
-
-private lateinit var auth: FirebaseAuth
-private lateinit var googleSignInClient: GoogleSignInClient
-
-
+    private lateinit var binding: ActivityHomeBinding
+    private lateinit var googleMap: GoogleMap
+    private lateinit var auth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
 
 
-class Home : AppCompatActivity() {
+    companion object {
+        const val MAP_LEVEL = 16f
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
 
 
         //PIN MY LOCATION BUTTON
         binding.pinMyLocationButton.setOnClickListener {
             val intent = Intent(this, PinMyLocation::class.java)
             startActivity(intent)
+
         }
+
+
 
         // Initialize and assign variable
         var bottomNavigationView: BottomNavigationView = binding.bottomNavView
 
         // Initialize and assign variable
-       val selectedItem = bottomNavigationView.selectedItemId
+        val selectedItem = bottomNavigationView.selectedItemId
         Toast.makeText(applicationContext, selectedItem.toString(), Toast.LENGTH_SHORT).show()
 
         bottomNavigationView.setOnNavigationItemSelectedListener(navBarWhenClicked)
 
     }
 
-        private val navBarWhenClicked= BottomNavigationView.OnNavigationItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.home -> {
-                    return@OnNavigationItemSelectedListener false
-                }
-                R.id.tools -> {
-                    Toast.makeText(applicationContext, "tools", Toast.LENGTH_SHORT).show()
-                    return@OnNavigationItemSelectedListener true
-                }
-                R.id.info -> {
-                    Toast.makeText(applicationContext, "info", Toast.LENGTH_SHORT).show()
-
-                    return@OnNavigationItemSelectedListener true
-                }
-                R.id.profile -> {
-                    Toast.makeText(applicationContext, "profile", Toast.LENGTH_SHORT).show()
-                    val intent = Intent(this, Profile::class.java)
-                    startActivity(intent)
-
-                    return@OnNavigationItemSelectedListener true
-                }
-            }
-            false
-
-        }
+    override fun onMapReady(map: GoogleMap) {
+        googleMap = map
+        googleMap.uiSettings.isZoomControlsEnabled = true
 
 
     }
+
+    private fun pinLocation() {
+
+    }
+
+    private fun getCurrentLocation() {
+
+    }
+
+
+    private val navBarWhenClicked = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.home -> {
+                return@OnNavigationItemSelectedListener false
+            }
+
+            R.id.tools -> {
+                Toast.makeText(applicationContext, "tools", Toast.LENGTH_SHORT).show()
+                return@OnNavigationItemSelectedListener true
+            }
+
+            R.id.info -> {
+                Toast.makeText(applicationContext, "info", Toast.LENGTH_SHORT).show()
+
+                return@OnNavigationItemSelectedListener true
+            }
+
+            R.id.profile -> {
+                Toast.makeText(applicationContext, "profile", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, Profile::class.java)
+                startActivity(intent)
+
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+
+    }
+
+
+}
 
 
 
