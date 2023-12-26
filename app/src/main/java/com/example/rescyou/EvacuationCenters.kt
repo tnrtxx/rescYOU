a3c9996c5654b79f2cbdaad771db9be448169e9f
russelhanapi <russelaminkadraphanapi@iskolarngbayan.pup.edu.ph>
2023-12-26 12:02:16 +0800

    Evacuation Center init

---
+++ app/src/main/java/com/example/rescyou/EvacuationCenters.kt
@@ -0,0 +1,11 @@
+package com.example.rescyou
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EvacuationCenters : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_evacuation_centers)
+    }
+}
